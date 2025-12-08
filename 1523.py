a5c6be70631f66ff16767b0a13eba0e8b1ad9b55
bag1s3k <jetam.mravenec@gmail.com>
2025-12-08 14:34:56 +0100

    1523. Count Odd Numbers in Interval Range

---
+++ 1523.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def countOdds(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+
+        return (high + 1) // 2 - (low // 2)
+
+solution = Solution()
+print(solution.countOdds(3, 7))
+print(solution.countOdds(8, 9))
